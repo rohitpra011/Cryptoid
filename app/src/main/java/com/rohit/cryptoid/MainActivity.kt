f04b9822494674187742be0222a1bba0ffaf0d37
Rohit Prajapati <rp53563@gmail.com>
2023-09-17 01:22:56 +0530

    SearchBar enabled, Animation Added, Toast bug fixed.

--- app/src/main/java/com/rohit/cryptoid/MainActivity.kt
+++ app/src/main/java/com/rohit/cryptoid/MainActivity.kt
@@ -3,12 +3,16 @@ package com.rohit.cryptoid
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.rohit.cryptoid.databinding.ActivityMainBinding
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -24,7 +28,36 @@ class MainActivity : AppCompatActivity() {
         rvAdapter=RvAdapter(this,data)
         binding.Rv.layoutManager=LinearLayoutManager(this)
         binding.Rv.adapter=rvAdapter
+        binding.search.addTextChangedListener(object :TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val filterdata=ArrayList<Model>()
+                    for (item in data){
+                        if (item.name.lowercase(Locale.getDefault()).contains(p0.toString().lowercase(Locale.getDefault())))
+                        {
+                            filterdata.add(item)
+                        }
+                    }
+                if (filterdata.isEmpty()){
+                    Toast.makeText(this@MainActivity,"No data available",Toast.LENGTH_SHORT).show()
+                }else{
+                    rvAdapter.changeData(filterdata)
+                }
+
+            }
+
+
+        })
+
     }
+
     val apiData:Unit
         get() {
             val url="https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=99d6dd62-2908-4aee-8590-cb7f5ceb59ad"
@@ -33,6 +66,7 @@ class MainActivity : AppCompatActivity() {
                 @SuppressLint("NotifyDataSetChanged")
                 object:JsonObjectRequest(Method.GET,url,null,Response.Listener {
                         response ->
+                    binding.progressBar.isVisible =false
                     try {
                         val dataArray=response.getJSONArray("data")
                         for (i in 0 until dataArray.length() )
--- app/src/main/java/com/rohit/cryptoid/RvAdapter.kt
+++ app/src/main/java/com/rohit/cryptoid/RvAdapter.kt
@@ -2,11 +2,17 @@ package com.rohit.cryptoid
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import androidx.recyclerview.widget.RecyclerView
 import com.rohit.cryptoid.databinding.RvItemsBinding
 
 class RvAdapter(val context: Context, var data:ArrayList<Model>):RecyclerView.Adapter<RvAdapter.ViewHolder>(){
+   fun changeData(filterdata:ArrayList<Model>){
+       data=filterdata
+       notifyDataSetChanged()
+   }
     inner class ViewHolder(val binding: RvItemsBinding):RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,8 +25,14 @@ class RvAdapter(val context: Context, var data:ArrayList<Model>):RecyclerView.Ad
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        setAnimation(holder.itemView)
         holder.binding.name.text=data[position].name
         holder.binding.symbol.text=data[position].symbol
         holder.binding.price.text=data[position].price
+    }
+    fun setAnimation(view:View){
+        val anim=AlphaAnimation(0.0f,1.0f)
+        anim.duration=1000
+        view.startAnimation(anim)
     }
 }
